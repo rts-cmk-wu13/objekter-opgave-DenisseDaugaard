46104c766ecdeded460185d4d18e31025ae384ce
DenisseDaugaard <U2478723@rts-365.dk>
2025-02-04 09:02:10 +0100

    hero image added

--- js/index.js
+++ js/index.js
@@ -1,6 +1,13 @@
 // slå dig løs her... 
 
-
-
 //eksempel på at udskrive alle overskrifter i services i konsollen:
 services.forEach(service => console.log(service.headline))
+
+
+let heroDocument = document.querySelector(".hero")
+heroDocument.classList.add("no-columns", "hero__img")
+let heroImg = document.createElement("img")
+heroImg.src = hero.image
+heroDocument.append(heroImg)
+
+
